afa186e015e2f274bc98218575eefe45c611af3f
Danubio de Araújo <danubio.bwm@gmail.com>
2023-06-01 01:18:54 -0300

    function event import invoices

--- lambda/invoices/invoiceEventsFunction.ts
+++ lambda/invoices/invoiceEventsFunction.ts
@@ -26,11 +26,18 @@ export async function handler(
       if (record.dynamodb!.NewImage!.pk.S!.startsWith("#transaction")) {
         console.log("Invoice Transaction event Received");
       } else {
-        console.log("Invoice event Received")
-        promises.push(createEvent(record.dynamodb!.NewImage!, "INVOICE_CREATED"));
+        console.log("Invoice event Received");
+        promises.push(
+          createEvent(record.dynamodb!.NewImage!, "INVOICE_CREATED")
+        );
       }
     } else if (record.eventName === "MODIFY") {
+
     } else if (record.eventName === "REMOVE") {
+      if (record.dynamodb!.OldImage!.pk.S === "#transaction") {
+        console.log("Invoice Transaction event Received");
+        promises.push(processExpiredTransaction(record.dynamodb!.OldImage!));
+      }
     }
   });
 
@@ -39,28 +46,52 @@ export async function handler(
   return;
 }
 
-async function createEvent(invoiceImage: { [key: string]: AttributeValue}, eventType: string):Promise<void> {
+async function createEvent(
+  invoiceImage: { [key: string]: AttributeValue },
+  eventType: string
+): Promise<void> {
+  const timestamp = new Date();
+  const ttl = ~~(timestamp.getTime() / 1000 + 60 * 60);
 
-  const timestamp = new Date()
-  const ttl = ~~(timestamp.getTime() / 1000 + 60 * 60)
+  await ddbClient
+    .put({
+      TableName: eventsDdb,
+      Item: {
+        pk: `#invoice_${invoiceImage.pk.S}`,
+        sk: `${eventType}#${timestamp}`,
+        ttl: ttl,
+        email: invoiceImage.pk.S!.split("_")[1],
+        createdAt: timestamp,
+        eventType: eventType,
+        info: {
+          transaction: invoiceImage.transactionId.S,
+          productId: invoiceImage.productId.S,
+          quantity: invoiceImage.quantity.N,
+        },
+      },
+    })
+    .promise();
 
-  await ddbClient.put({
-    TableName: eventsDdb,
-    Item: {
-      pk: `#invoice_${invoiceImage.pk.S}`,
-      sk: `${eventType}#${timestamp}`,
-      ttl: ttl,
-      email: invoiceImage.pk.S!.split("_")[1],
-      createdAt: timestamp,
-      eventType: eventType,
-      info:{
-        transaction: invoiceImage.transactionId.S,
-        productId: invoiceImage.productId.S,
-        quantity: invoiceImage.quantity.N,
-      }
-    },
-  }).promise();
+  return;
+}
 
-return
+async function processExpiredTransaction(
+  invoiceTransactionImage: { [key: string]: AttributeValue }): Promise<void> {
+
+  const transactionId = invoiceTransactionImage.sk.S!
+  const connectionId = invoiceTransactionImage.connectionId.S!
+
+  console.log(`transactionId ${transactionId} - ConnectionId ${connectionId}`)
+
+  if(invoiceTransactionImage.transactionStatus.S === "INVOICE_PROCESSED"){
+    console.log("Invoice Processed")
+  }else{
+    console.log(`Invoice Import failed - Status: ${invoiceTransactionImage.transactionStatus.S}`)
+    await invoiceWSService.sendInvoiceStatus(transactionId, connectionId, "TIMEOUT")
+
+    await invoiceWSService.disconnectClient(connectionId)
+  }
+
+
 }
 
